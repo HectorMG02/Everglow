f9bcd9f8ddb89a47f1ea188234687cec1841dd35
Hector <hector.matias@ecam.es>
2024-05-09 00:35:01 +0200

    toggle mode working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: { },
+    extend: {
+      colors: {
+        light: {
+          DEFAULT: 'rgb(100, 105, 142)',
+          STRONG: 'rgb(0, 12, 96)',
+        },
+        dark: {
+          DEFAULT: '#ffff',
+          STRONG: '#ffff',
+        },
+      },
+    },
   },
   darkMode: 'class',
   plugins: [],
