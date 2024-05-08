b610589bfeea339344a1e71894292e715004ab31
Hector <hector.matias@ecam.es>
2024-05-08 12:07:27 +0200

    3 words animated

--- src/components/AnimatedWord.tsx
+++ src/components/AnimatedWord.tsx
@@ -19,7 +19,7 @@ const AnimatedWord = ({ words }: { words: string[] }) => {
 
     const wordVariants = {
         initial: { opacity: 0, y: 20 },
-        animate: { opacity: 1, y: 0, transition: { duration: 0.25 } },
+        animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
         exit: { opacity: 0, y: -20, transition: { duration: 0.25 } }
     };
 
